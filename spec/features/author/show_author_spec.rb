0527b4fbcc3630afefb58f2ed0c4397d0874edab
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-23 18:15:24 +0100

    Failing Test for #6

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Show Author Page", type :feature do
+  it "should exist at 'author_path', render without error and show author details" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_text(@alan.first_name)
+    expect(page).to have_text(@alan.last_name)
+    expect(page).to have_text(@alan.homepage)
+  end
+end
