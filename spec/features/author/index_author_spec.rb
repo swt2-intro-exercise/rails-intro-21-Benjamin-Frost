f158deb0731b55457c200208b732d118b4881253
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-23 18:35:19 +0100

    Failing Test for #7

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Index author page", type: :feature do
+  it "should exist at 'authors_path' and render without an error and contain the list of authors" do
+    visit authors_path
+
+    expect(page).to have_table
+    expect(page).to have_css('th', text: 'Name')
+    expect(page).to have_css('th', text: 'Homepage')
+  end
+
+  it "should contain a link to create a new author" do
+    visit authors_path
+    expect(page).to have_link 'New', href: new_author_path
+  end
+end
