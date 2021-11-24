a49eb853923138f0cd8e0e89689fa98726776180
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-24 22:28:09 +0100

    Failing Test for #9

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -21,4 +21,10 @@ describe "New author page", type: :feature do
     page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
     find('input[type="submit"]').click
   end
+
+  it "should show an error message when last_name is missing" do
+    visit new_author_path
+    find('input[type="submit"]').click
+    expect(page).to have_text('error')
+  end
 end
