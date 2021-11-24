0efa52fccc9c225cc58854cac90de5a8125beca9
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-24 23:22:37 +0100

    Add Author Edit Page (Closes #10)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -13,10 +13,25 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
+    
     if @author.save
-      redirect_to root_path, notice: 'Success!'
+      redirect_to @author
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+   
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
     end
   end
 
