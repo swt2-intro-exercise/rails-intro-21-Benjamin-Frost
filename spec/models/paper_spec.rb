d9f9c8a5070085cfa155c80d7fa90ead37feae77
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-25 12:48:00 +0100

    Failing Test for #14

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Paper, type: :model do
     expect(paper.venue).to eq("Best Venue World")
     expect(paper.year).to eq(1930)
   end
+
+  it "should not be valid without having all attributes set" do
+    paper = Paper.new()
+    expect(paper).to_not be_valid
+
+    paper.title = "Some Title"
+    expect(paper).to_not be_valid
+
+    paper.vanue = "Some Venue"
+    expect(paper).to_not be_valid
+
+    paper.year = "Some Year"
+    expect(paper).to_not be_valid
+
+    paper.year = 1999
+    expect(paper).to be_valid
+  end
 end
