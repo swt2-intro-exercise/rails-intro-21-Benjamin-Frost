4ccb0316285ddfede3eddccf153a9d87290a4131
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-25 12:00:00 +0100

    Failing Test for #11

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -13,4 +13,9 @@ describe "Index author page", type: :feature do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
   end
+
+  it "should contain a link to edit an author" do
+    visit authors_path
+    expect(page).to have_link 'Edit', href: edit_author_path
+  end
 end
