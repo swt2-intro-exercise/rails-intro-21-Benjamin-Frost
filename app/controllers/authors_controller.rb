cab1509073c2336692e46e708b38dac3ebc5946e
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-25 19:48:44 +0100

    Refactor Authors Controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,48 +1,57 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  # GET /authors
   def index
     @authors = Author.all
   end
 
+  # GET /authors/1
   def show
-   @author = Author.find(params[:id]) 
   end
 
+  # GET /authors/new
   def new
     @author = Author.new
   end
 
+  # GET /authors/1/edit
+  def edit
+  end
+
+  # POST /authors
   def create
     @author = Author.new(author_params)
     
     if @author.save
-      redirect_to @author
+      redirect_to @author, notice: 'Author was successfully created.'
     else
-      render 'new'
+      render :new
     end
   end
 
-  def edit
-    @author = Author.find(params[:id])
-  end
-
+  # PATCH/PUT /authors/1
   def update
-    @author = Author.find(params[:id])
-   
     if @author.update(author_params)
-      redirect_to @author
+      redirect_to @author, notice: 'Author was successfully updated.'
     else
-      render 'edit'
+      render :edit
     end
   end
 
+  # DELETE /papers/1
   def destroy
-    @author = Author.find(params[:id])
     @author.destroy
-    
-    redirect_to authors_path
+    redirect_to authors_path, notice: 'Author was successfully destroyed.'
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
     def author_params
       params.require(:author).permit(:first_name, :last_name, :homepage)
     end
