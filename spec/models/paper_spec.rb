0c3c7575edebcc923957d249046ff2acbae0fa58
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-25 12:38:09 +0100

    Adapt Tests generated by Scaffold

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  paper = Paper.new(
+    title: "Best Paper World",
+    venue: "Best Venue World",
+    year: 1930
+  )
+
+  it "attributes set properly" do
+    expect(paper.title).to eq("Best Paper World")
+    expect(paper.venue).to eq("Best Venue World")
+    expect(paper.year).to eq(1930)
+  end
 end
