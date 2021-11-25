208b05bb60c663624d8ffb4442ab560786b18c44
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-25 15:08:53 +0100

    Authors Papers ManyToMany (Closes #15)

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,6 @@
 class Paper < ApplicationRecord
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true }
+  
+  has_and_belongs_to_many :authors
 end
